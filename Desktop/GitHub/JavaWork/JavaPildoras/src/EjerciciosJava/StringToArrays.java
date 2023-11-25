4b8600a4915ece6b41fee4dc17735731064155f5
Paola9629 <paolauv96@gmail.com>
2023-11-25 23:40:32 +0100

    "Añadido"

--- Desktop/GitHub/JavaWork/JavaPildoras/src/EjerciciosJava/StringToArrays.java
+++ Desktop/GitHub/JavaWork/JavaPildoras/src/EjerciciosJava/StringToArrays.java
@@ -4,7 +4,7 @@ public class StringToArrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String alumno1="Hola";
+		String alumno1="Hola Pepe";
 		
 		String alumno1_resumen=alumno1.substring(1, 3);
 		
