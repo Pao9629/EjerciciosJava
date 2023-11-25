a96644cd2731130dd367950bfa59c28cea906316
Paola9629 <paolauv96@gmail.com>
2023-11-25 23:57:13 +0100

    "Actualización de ejercicio"

---
+++ Desktop/GitHub/JavaWork/JavaPildoras/src/EjerciciosJava/EntradaEjemplo2.java
@@ -0,0 +1,20 @@
+package EjerciciosJava;
+
+
+import javax.swing.*;
+public class EntradaEjemplo2 {
+
+	public static void main(String[] args) {
+		// 
+		
+		String nombre_usuario=JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+		
+		String edad=JOptionPane.showInputDialog("Introduce la edad, por favor");
+		
+		int edad_usuario=Integer.parseInt(edad);
+		
+		System.out.println("Hola " + nombre_usuario + ". En 5 años tendrás "+ (edad_usuario+5) + " años");
+
+	}
+
+}
