8d0b8a6bb10a8a31b1ff99410df8976212840f25
Paola9629 <paolauv96@gmail.com>
2023-11-26 18:48:48 +0100

    "Nuevas actividades de Condicionales y bucles"

---
+++ Desktop/GitHub/JavaWork/JavaPildoras/src/EjerciciosJava/EjercicioCondicionalfElse.java
@@ -0,0 +1,45 @@
+package EjerciciosJava;
+
+import java.util.Scanner;
+
+public class EjercicioCondicionalfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+			Scanner entrada=new Scanner(System.in);
+			
+			System.out.println("Introduce tu edad, por favor");
+			
+			int edad = entrada.nextInt();
+			
+			// Si quiero por ejemplo imprimir varias condicionales if
+			// porque quiero que me imprima diversos resultados dependiendo
+			// del numero introducido tengo opcion de escribir varias 
+			// hacer un metodo else if
+			
+			if (edad<18) {
+				
+				System.out.println("Eres adolescente");
+			}
+			
+			else if (edad<40) {
+			
+				System.out.println("Eres joven");
+			
+			}
+			
+			else if (edad<65) { // y sino mira a ver ... 
+				
+				System.out.println("Eres maduro");
+			
+			}
+			
+			else {
+				
+				System.out.println("Cuidate");
+			
+			}
+	}
+
+}
---
+++ Desktop/GitHub/JavaWork/JavaPildoras/src/EjerciciosJava/EstructurasCondicionalesyBucles.java
@@ -0,0 +1,35 @@
+package EjerciciosJava;
+
+import java.util.*;
+
+public class EstructurasCondicionalesyBucles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Introduce tu edad");
+		
+		int edad = entrada.nextInt();
+		
+		if (edad>=18) {
+			System.out.print("Eres mayor de edad");
+		// Aquí esta detenido el programa pero ya hemos empezado con la primera
+		//	condición, para completar ahora toca poner else
+		// if sería como "si" y else sería como " y si no es verdad"
+			//podemos tener if sin else , pero NO else sin if
+		
+		}
+		//podemos ponerlo a continuación o debajo 
+		
+		else {
+			
+			System.out.println("Eres menor de edad");
+			
+		} //cierre del else
+		
+		
+	} // llave metodo main
+
+} //llave cierre de la clase
